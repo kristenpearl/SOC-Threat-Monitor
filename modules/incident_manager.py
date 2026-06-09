66739d5fb0ded6e1b6b73ffb9c2e52e475db81e9
Chioma Precious Ogbonna <155928331+kristenpearl@users.noreply.github.com>
2026-06-09 15:05:30 +0100

    Create incident_manager.py

---
+++ modules/incident_manager.py
@@ -0,0 +1,19 @@
+def get_incidents():
+
+    return [
+
+        {
+            "id":"INC-001",
+            "title":"Brute Force Login Attempt",
+            "severity":"Critical",
+            "status":"Open"
+        },
+
+        {
+            "id":"INC-002",
+            "title":"Suspicious IP Activity",
+            "severity":"High",
+            "status":"Investigating"
+        }
+
+    ]
