3efad1be28e0430e14ab1c0b1507dadad70d655a
Chioma Precious Ogbonna <155928331+kristenpearl@users.noreply.github.com>
2026-06-09 15:22:28 +0100

    Add MITRE attack techniques mapping

--- modules/mitre_mapping.py
+++ modules/mitre_mapping.py
@@ -1,1 +1,43 @@
+ATTACK_TECHNIQUES = {
 
+    "Brute Force Login Attempt": {
+
+        "id": "T1110",
+
+        "name": "Brute Force"
+
+    },
+
+    "Suspicious Account Access": {
+
+        "id": "T1078",
+
+        "name": "Valid Accounts"
+
+    },
+
+    "Command Execution": {
+
+        "id": "T1059",
+
+        "name": "Command and Scripting Interpreter"
+
+    }
+
+}
+
+def get_mitre_mapping(incident):
+
+    return ATTACK_TECHNIQUES.get(
+
+        incident,
+
+        {
+
+            "id":"Unknown",
+
+            "name":"Unknown"
+
+        }
+
+    )
