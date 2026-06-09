986034ced95bd96d2a06ba21dc66246357968877
Chioma Precious Ogbonna <155928331+kristenpearl@users.noreply.github.com>
2026-06-09 15:26:41 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+DATABASE = "soc.db"
+
 def init_db():
 
-    conn = sqlite3.connect("soc.db")
+    conn = sqlite3.connect(DATABASE)
 
     cursor = conn.cursor()
 
@@ -25,3 +27,74 @@ def init_db():
     conn.commit()
 
     conn.close()
+
+
+def add_incident(
+
+    title,
+
+    severity,
+
+    status
+
+):
+
+    conn = sqlite3.connect(DATABASE)
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+
+        """
+
+        INSERT INTO incidents
+
+        (
+
+            title,
+
+            severity,
+
+            status
+
+        )
+
+        VALUES
+
+        (?, ?, ?)
+
+        """,
+
+        (
+
+            title,
+
+            severity,
+
+            status
+
+        )
+
+    )
+
+    conn.commit()
+
+    conn.close()
+
+
+def get_incidents():
+
+    conn = sqlite3.connect(DATABASE)
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+
+        "SELECT * FROM incidents"
+    )
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
