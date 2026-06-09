9849a0226746d7856e33694dc5d3ec015efcb290
Chioma Precious Ogbonna <155928331+kristenpearl@users.noreply.github.com>
2026-06-09 15:03:18 +0100

    Initialize SQLite database and create incidents table

--- database.py
+++ database.py
@@ -1,1 +1,27 @@
+import sqlite3
 
+def init_db():
+
+    conn = sqlite3.connect("soc.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+
+    CREATE TABLE IF NOT EXISTS incidents(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        title TEXT,
+
+        severity TEXT,
+
+        status TEXT
+
+    )
+
+    """)
+
+    conn.commit()
+
+    conn.close()
